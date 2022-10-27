41eb20d44ee2185b44bd5154e870125af49eeba2
dhwilliams0 <dhwilliams3007@gmail.com>
2022-10-27 19:15:16 +0000

    rr

---
+++ resources folder.1/resources.js
@@ -0,0 +1,41 @@
+// var video = document.getElementById("myVideo");
+// var btn = document.getElementById("myBtn");
+
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "Pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "Play";
+//   }
+// }
+
+x.play();
+playAudio();
+
+// SCRIPT FOR SOUND 
+
+// $(document).ready(function(){
+//   $("#hide").click(function(){
+//     $("#hide").hide();
+//   });
+//   $("#show").click(function(){
+//     $("#hide").show();
+//   });
+// });
+
+
+
+
+
+
+// var x = document.getElementById("myAudio"); 
+
+// function playAudio() { 
+//   x.play(); 
+// } 
+
+// function pauseAudio() { 
+//   x.pause(); 
+// } 
